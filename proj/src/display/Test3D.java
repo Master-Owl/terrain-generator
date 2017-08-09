efed38193cb35e8dc1bab8123501f9740e14ddc6
Trevor Howell <tkhowell95@gmail.com>
2017-08-09 16:29:03 -0600

    Cleaned up code n' stuff

--- proj/src/display/Test3D.java
+++ proj/src/display/Test3D.java
@@ -1,17 +1,13 @@
 package display;
 
 import java.applet.Applet;
-import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Container;
-import java.awt.Frame;
-import java.awt.Graphics2D;
 import java.awt.GraphicsConfiguration;
 import java.awt.Label;
 import java.awt.Panel;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -19,10 +15,6 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.image.BufferedImage;
-
 import javax.media.j3d.AmbientLight;
 import javax.media.j3d.Appearance;
 import javax.media.j3d.BoundingSphere;
@@ -31,8 +23,7 @@ import javax.media.j3d.Canvas3D;
 import javax.media.j3d.ColoringAttributes;
 import javax.media.j3d.DirectionalLight;
 import javax.media.j3d.Material;
-import javax.media.j3d.PickInfo;
-import javax.media.j3d.PolygonAttributes;
+import javax.media.j3d.Node;
 import javax.media.j3d.Shape3D;
 import javax.media.j3d.Texture;
 import javax.media.j3d.Texture2D;
@@ -42,25 +33,20 @@ import javax.media.j3d.TransformGroup;
 import javax.swing.Timer;
 import javax.vecmath.Color3f;
 import javax.vecmath.Color4f;
-import javax.vecmath.Matrix3d;
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
 import javax.vecmath.Vector3f;
 
-import com.sun.j3d.utils.applet.MainFrame;
-import com.sun.j3d.utils.behaviors.mouse.MouseRotate;
 import com.sun.j3d.utils.behaviors.vp.OrbitBehavior;
 import com.sun.j3d.utils.geometry.Box;
-import com.sun.j3d.utils.geometry.ColorCube;
 import com.sun.j3d.utils.geometry.Cone;
 import com.sun.j3d.utils.geometry.Cylinder;
 import com.sun.j3d.utils.geometry.Primitive;
 import com.sun.j3d.utils.geometry.Sphere;
 import com.sun.j3d.utils.image.TextureLoader;
-import com.sun.j3d.utils.pickfast.PickCanvas;
-import com.sun.j3d.utils.picking.PickResult;
 import com.sun.j3d.utils.universe.SimpleUniverse;
 
+@SuppressWarnings("serial")
 public class Test3D extends Applet implements ActionListener, KeyListener, MouseListener, MouseMotionListener {
 	private SimpleUniverse universe;
 	private BranchGroup group;
@@ -79,23 +65,23 @@ public class Test3D extends Applet implements ActionListener, KeyListener, Mouse
 	private float sign = 1.0f; // going up or down
 	private Timer timer;
 	private float xloc = 0.0f;
-	
+
 	// Rotation vars
 	private int x_pos;
 	private int y_pos;
 
 	public Test3D() {
 		lightDir = new Vector3f(4.0f, -7.0f, -12.0f);
-		bounds  = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 100.0);
+		bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 100.0);
 		white = new Color3f(1.0f, 1.0f, 1.0f);
 		black = new Color3f(0, 0, 0);
 		green = new Color3f(0.0f, 0.65f, 0.1f);
 		red = new Color3f(1.0f, 0.0f, 0.2f);
-		
+
 		x_pos = 0;
 		y_pos = 0;
 		objTrans = new TransformGroup();
-		
+
 		setLocation(500, 500);
 	}
 
@@ -309,36 +295,39 @@ public class Test3D extends Applet implements ActionListener, KeyListener, Mouse
 	}
 
 	// ----------------------------------------------
-	
-	// Rotation code snippet taken from http://www.java3d.org/miceandmen.html
-	public void interactions(){		
+
+	// Rotation code snippet mostly taken from
+	// http://www.java3d.org/miceandmen.html
+	// OrbitBehavior taken from
+	// https://stackoverflow.com/questions/17464571/rotating-a-3d-view-with-mouse-movements-with-a-fixed-camera
+	public void interactions() {
 		setLayout(new BorderLayout());
-		
+
 		GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
 		Canvas3D canvas = new Canvas3D(config);
-		add("Center", canvas);		
+		add("Center", canvas);
 		universe = new SimpleUniverse(canvas);
 		group = new BranchGroup();
-		
+
 		TextureAttributes attr = new TextureAttributes();
 		Appearance ap = getAppearance(new Color3f(Color.blue));
 		attr.setTextureMode(TextureAttributes.MODULATE);
 		ap.setTextureAttributes(attr);
 		ap.setMaterial(new Material(green, black, green, white, 70f));
 		Box box = new Box(0.5f, 0.5f, 0.5f, Primitive.GENERATE_TEXTURE_COORDS, ap);
-		box.setCapability(Box.ENABLE_APPEARANCE_MODIFY);
-		box.setCapability(Box.GEOMETRY_NOT_SHARED);
-		box.setCapability(Box.ALLOW_LOCAL_TO_VWORLD_READ);
-		
+		box.setCapability(Primitive.ENABLE_APPEARANCE_MODIFY);
+		box.setCapability(Primitive.GEOMETRY_NOT_SHARED);
+		box.setCapability(Node.ALLOW_LOCAL_TO_VWORLD_READ);
+
 		Shape3D frontShape = box.getShape(Box.FRONT);
 		frontShape.setAppearance(ap);
 
 		box.getShape(Box.TOP).setAppearance(getAppearance(new Color3f(Color.magenta)));
-		box.getShape(Box.BOTTOM).setAppearance(getAppearance(new Color3f(Color.orange)));		
+		box.getShape(Box.BOTTOM).setAppearance(getAppearance(new Color3f(Color.orange)));
 		box.getShape(Box.RIGHT).setAppearance(getAppearance(new Color3f(Color.red)));
 		box.getShape(Box.LEFT).setAppearance(getAppearance(new Color3f(Color.green)));
-		box.getShape(Box.BACK).setAppearance(getAppearance(new Color3f(Color.yellow)));	
-		
+		box.getShape(Box.BACK).setAppearance(getAppearance(new Color3f(Color.yellow)));
+
 		OrbitBehavior behavior = new OrbitBehavior(canvas, OrbitBehavior.REVERSE_ROTATE);
 		BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 100.0);
 		objTrans = new TransformGroup();
@@ -346,29 +335,32 @@ public class Test3D extends Applet implements ActionListener, KeyListener, Mouse
 		behavior.setSchedulingBounds(bounds);
 		objTrans.addChild(box);
 		group.addChild(objTrans);
-		
+
 		universe.getViewingPlatform().setViewPlatformBehavior(behavior);
 		universe.getViewingPlatform().setNominalViewingTransform();
 		universe.addBranchGraph(group);
 	}
-	
+
 	public Appearance getAppearance(Color3f color) {
 		Appearance ap = new Appearance();
 		Texture texture = new Texture2D();
 		TextureAttributes texAttr = new TextureAttributes();
+		Material mat = new Material(color, black, color, white, 70f);
+		ColoringAttributes ca = new ColoringAttributes(color, ColoringAttributes.NICEST);
+
 		texAttr.setTextureMode(TextureAttributes.MODULATE);
 		texture.setBoundaryModeS(Texture.WRAP);
 		texture.setBoundaryModeT(Texture.WRAP);
 		texture.setBoundaryColor(new Color4f(0.0f, 1.0f, 0.0f, 0.0f));
-		Material mat = new Material(color, black, color, white, 70f);
+
 		ap.setTextureAttributes(texAttr);
 		ap.setMaterial(mat);
 		ap.setTexture(texture);
-		ColoringAttributes ca = new ColoringAttributes(color, ColoringAttributes.NICEST);
 		ap.setColoringAttributes(ca);
+
 		return ap;
 	}
-	
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
 
@@ -400,22 +392,22 @@ public class Test3D extends Applet implements ActionListener, KeyListener, Mouse
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		
+
 		// Rotate a flat image
-//		int dx = e.getX() - x_pos;
-//		int dy = e.getY() - y_pos;
-//		x_pos = e.getX();
-//		y_pos = e.getY();
-//		
-//		double rotation = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-//		
-//		Transform3D transform = new Transform3D();
-//		Matrix3d matrix = new Matrix3d();
-//		objTrans.getTransform(transform);
-//		transform.getRotationScale(matrix);
-//		transform.rotZ(rotation);
-//		objTrans.setTransform(transform);
-//		
+		// int dx = e.getX() - x_pos;
+		// int dy = e.getY() - y_pos;
+		// x_pos = e.getX();
+		// y_pos = e.getY();
+		//
+		// double rotation = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+		//
+		// Transform3D transform = new Transform3D();
+		// Matrix3d matrix = new Matrix3d();
+		// objTrans.getTransform(transform);
+		// transform.getRotationScale(matrix);
+		// transform.rotZ(rotation);
+		// objTrans.setTransform(transform);
+		//
 	}
 
 	@Override
